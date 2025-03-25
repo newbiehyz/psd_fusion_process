ebbe0945b928a9cfe25a51f7a0d1d1e64d866e72
hongyi_zhu <hongyi_zhu@apa.patac>
2025-03-25 11:02:00 +0800

    add psd2vcu timestamp update

--- psd_fusion_process_template.cpp
+++ psd_fusion_process_template.cpp
@@ -500,7 +500,7 @@ tResult cpsd_fusion_process::TimeTrigger_thread_100ms_1()
     
     auto start = std::chrono::steady_clock::now(); // 用于计算TIMECOST
 
-    LOGD("PSD Version: 03241837 emos9 add psd2vcu timestamp. ENABLE: slottype fix, occupy realtime update, displayID fix, remove overlapped slots. DISABLE: psd2vcu fixed");
+    LOGD("PSD Version: 03251101 emos9 add psd2vcu timestamp. ENABLE: slottype fix, occupy realtime update, displayID fix, remove overlapped slots. DISABLE: psd2vcu fixed");
     
     // part2 输入，上游：RD, DR, USS, peception, VCU select ID, statemachine
 
