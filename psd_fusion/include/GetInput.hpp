3caeefcb9a56516e88a197c65841bc0e9ee996e3
hongyi_zhu <hongyi_zhu@apa.patac>
2025-05-15 19:43:42 +0800

    0515 release

---
+++ psd_fusion/include/GetInput.hpp
@@ -0,0 +1,67 @@
+#ifndef PARKINGSYSTEM_HPP
+#define PARKINGSYSTEM_HPP
+
+#include <vector>
+#include "psd_fusion_process_header.h"
+#include "psd_fusion_process_template.h"
+#include "apa_define.h"
+#include <deque>
+
+struct DRPoseWithTime {
+    Loc::App2emap_DR dr_pose;
+    unsigned long long timestamp;
+};
+
+extern std::deque<DRPoseWithTime> dr_pose_buffer;
+extern const size_t MAX_BUFFER_SIZE;
+extern const size_t MAX_RD_DR_ALLOWANCE;
+
+void UpdateDRPoseBuffer(const Loc::App2emap_DR& dr_pose);
+bool GetMatchedDRPose(unsigned long long rd_timestamp, Loc::App2emap_DR& matched_pose);
+
+
+
+
+class GetInput {
+public:
+    GetInput();
+
+    ~GetInput();
+
+    void GetRDInfo(int& apa_status, rd::QuadParkingSlots& rd_info, unsigned long long& singleframeslotsID, std::vector<padVisionSlotCoord>& singleframeslots);
+    void GetDRInfo(int& apa_status, Loc::App2emap_DR& dr_pose, Loc::App2emap_DR& previous_dr_pose, padVehiclePose& pose_globaldata, bool& is_Still, int& still_count);
+    void GetPerception(Fus::PkEmapObs& obs_info_get);
+    void GetAPAStatus(StatusDecOutput& apastatus_info, int& apa_status);
+    void GetSearchParkStatus(StatusDecFusionOutput& searchpark_info, int& park_request, int& search_interrupt);
+
+    void ClearExistedInput(unsigned long long& singleframeslotsID, std::vector<padVisionSlotCoord>& singleframeslots, padVehiclePose& pose_globaldata, int& apa_status);
+
+    void GetAllInput();
+
+
+
+
+    rd::QuadParkingSlots rd_info;
+    unsigned long long singleframeslotsID;
+    std::vector<padVisionSlotCoord> singleframeslots;
+    Loc::App2emap_DR dr_pose;
+    padVehiclePose pose_globaldata;
+    Loc::App2emap_DR previous_dr_pose;
+    bool is_Still;
+    int still_count;
+    Fus::PkEmapObs obs_info_get;
+    StatusDecOutput apastatus_info;
+    int apa_status;
+    StatusDecFusionOutput searchpark_info;
+    int park_request;
+    int search_interrupt;
+
+
+
+
+private:
+
+
+};
+
+#endif // PARKINGSYSTEM_HPP
---
+++ psd_fusion/src/utils.cpp
@@ -0,0 +1,199 @@
+#include "utils.h"
+
+void ClearRD(rd::QuadParkingSlots& rd_info) {
+    rd_info = rd::QuadParkingSlots{};
+    LOGD("CLEAR rd_info");
+}
+
+void ClearDR(Loc::App2emap_DR& dr_pose, padVehiclePose& pose_globaldata) {
+    dr_pose = Loc::App2emap_DR{};       
+    pose_globaldata = padVehiclePose{};  
+    LOGD("CLEAR dr_pose and pose_globaldata");
+}
+
+void ClearOBS(Fus::PkEmapObs& obs_info_get) {
+    obs_info_get = Fus::PkEmapObs{};
+    LOGD("CLEAR obs_info_get");
+}
+
+void ClearUSS(UssIf_stPLVOutputInfo_t& uss_info, UssIf_stPLVOutputInfo_t& uss_restruct) {
+    uss_info = UssIf_stPLVOutputInfo_t{};
+    uss_restruct = UssIf_stPLVOutputInfo_t{};
+    LOGD("CLEAR uss_info");
+}
+
+void ClearParkingSlots(std::vector<padVisionSlotCoord>& singleframeslots, unsigned long long& singleframeslotsID, apaSlotListInfo& outputSlot_VIS, apaSlotListInfo& outputSlot_USS, apaSlotListInfo& outputSlot_FUSED, int& parkout_flag) {
+    singleframeslots.clear();
+    singleframeslotsID = 0;
+    outputSlot_VIS.slots_in_cur_frame.clear();
+    outputSlot_USS.slots_in_cur_frame.clear();
+    outputSlot_FUSED.slots_in_cur_frame.clear();
+    parkout_flag = 0;
+    LOGD("CLEAR singleframeslots, size: %d, parkout_flag = %d", singleframeslots.size(), parkout_flag);
+}
+
+bool CheckTimeSync(uint64_t current1970_ms, uint64_t rd_timestamp, uint64_t dr_timestamp) {
+    if (std::llabs(current1970_ms - rd_timestamp) > 1500 || std::llabs(current1970_ms - dr_timestamp) > 1500) {
+        LOGW("[TIMESYNC] current: %llu, RD timestamp: %llu, DR timestamp: %llu, Time synchronization not met, skipping execution.", current1970_ms, rd_timestamp, dr_timestamp);
+        return false;
+    }
+    LOGD("[TIMESYNC] current: %llu, RD timestamp: %llu, DR timestamp: %llu, Time synchronization achieved!", current1970_ms, rd_timestamp, dr_timestamp);
+    return true;
+}
+
+bool CheckRDFrameTimestamp(uint64_t currentTimestamp) {
+    static uint64_t previousTimestamp = 0;  // 存储上一个周期的时间戳
+
+    if (currentTimestamp - previousTimestamp > 5) {
+        LOGW("[RDDelay] **************PASS************** currentTimestamp: %llu, previousTimestamp: %llu",currentTimestamp, previousTimestamp);
+        // 更新上一个时间戳
+        previousTimestamp = currentTimestamp;
+        return true;
+    }
+    else{
+        LOGW("[RDDelay] **************DELAY************** currentTimestamp: %llu, previousTimestamp: %llu",currentTimestamp, previousTimestamp);
+        // 更新上一个时间戳
+        previousTimestamp = currentTimestamp;
+        return false;
+    }
+}
+
+void LogSlotInfo(const apaSlotListInfo& slots, const std::string& slotType) {
+    for (const auto& psd_m_output : slots.slots_in_cur_frame) {
+        if (slotType == "ORIGIN VISSLOTS") {
+            LOGD("[%s] TOTAL SLOT NUM: %d, Slot#%d, type: %d, occ: %d, StopDis: %f, StopLoc: %d, Stopper: (%d, %d) (%d, %d). (%d, %d) (%d, %d) (%d, %d) (%d, %d)",
+                 slotType.c_str(),
+                 slots.slots_in_cur_frame.size(),
+                 psd_m_output.rectInfo.label,
+                 psd_m_output.rectInfo.PStype,
+                 psd_m_output.rectInfo.iSodType,
+                 psd_m_output.rectInfo.StopperDistance,
+                 psd_m_output.rectInfo.StopperLocation,
+                 psd_m_output.rectInfo.StopperX[0],
+                 psd_m_output.rectInfo.StopperY[0],
+                 psd_m_output.rectInfo.StopperX[1],
+                 psd_m_output.rectInfo.StopperY[1],
+                 psd_m_output.rectInfo.pt[0].x,
+                 psd_m_output.rectInfo.pt[0].y,
+                 psd_m_output.rectInfo.pt[1].x,
+                 psd_m_output.rectInfo.pt[1].y,
+                 psd_m_output.rectInfo.pt[2].x,
+                 psd_m_output.rectInfo.pt[2].y,
+                 psd_m_output.rectInfo.pt[3].x,
+                 psd_m_output.rectInfo.pt[3].y);
+        } else if (slotType == "ORIGIN USSSLOTS") {
+            LOGD("[%s] TOTAL SLOT NUM: %d, Slot#%d, type: %d, SOD: %d, DownSlotSOD: %d, iMinOtherSideDist: %d, iRoadEdgeDist: %d, (%d, %d) (%d, %d) (%d, %d) (%d, %d)",
+                 slotType.c_str(),
+                 slots.slots_in_cur_frame.size(),
+                 psd_m_output.rectInfo.label,
+                 psd_m_output.rectInfo.PStype,
+                 psd_m_output.rectInfo.iSodType,
+                 psd_m_output.rectInfo.iDownSlotSOD,
+                 psd_m_output.rectInfo.iMinOtherSideDist,
+                 psd_m_output.rectInfo.iRoadEdgeDist,
+                 psd_m_output.rectInfo.pt[0].x,
+                 psd_m_output.rectInfo.pt[0].y,
+                 psd_m_output.rectInfo.pt[1].x,
+                 psd_m_output.rectInfo.pt[1].y,
+                 psd_m_output.rectInfo.pt[2].x,
+                 psd_m_output.rectInfo.pt[2].y,
+                 psd_m_output.rectInfo.pt[3].x,
+                 psd_m_output.rectInfo.pt[3].y);
+        } else if (slotType == "FUSIONSLOTS") {
+            LOGD("[%s] TOTAL SLOT NUM: %d, Slot#%d, type: %d, occ: %d, StopDis: %f, StopLoc: %d, Material: %d, NotToRelease: %d, ParkInSlot: %d, Stopper: (%d, %d) (%d, %d). (%d, %d) (%d, %d) (%d, %d) (%d, %d)",
+                 slotType.c_str(),
+                 slots.slots_in_cur_frame.size(),
+                 psd_m_output.rectInfo.label,
+                 psd_m_output.rectInfo.PStype,
+                 psd_m_output.rectInfo.iSodType,
+                 psd_m_output.rectInfo.StopperDistance,
+                 psd_m_output.rectInfo.StopperLocation,
+                 psd_m_output.rectInfo.iMaterial,
+                 psd_m_output.rectInfo.NotToRelease,
+                 psd_m_output.rectInfo.ParkInSlot,
+                 psd_m_output.rectInfo.StopperX[0],
+                 psd_m_output.rectInfo.StopperY[0],
+                 psd_m_output.rectInfo.StopperX[1],
+                 psd_m_output.rectInfo.StopperY[1],
+                 psd_m_output.rectInfo.pt[0].x,
+                 psd_m_output.rectInfo.pt[0].y,
+                 psd_m_output.rectInfo.pt[1].x,
+                 psd_m_output.rectInfo.pt[1].y,
+                 psd_m_output.rectInfo.pt[2].x,
+                 psd_m_output.rectInfo.pt[2].y,
+                 psd_m_output.rectInfo.pt[3].x,
+                 psd_m_output.rectInfo.pt[3].y);
+        }
+    }
+}
+
+void LogWorldSlotInfo(const apaSlotListInfo& slots, const std::string& slotType) {
+    for (const auto& psd_m_output : slots.WorldoutRect) {
+        if (slotType == "ORIGIN VISSLOTS") {
+            LOGD("[%s World] TOTAL SLOT NUM: %d, Slot#%d, type: %d, occ: %d, StopDis: %f, StopLoc: %d, (%d, %d) (%d, %d) (%d, %d) (%d, %d)",
+                 slotType.c_str(),
+                 slots.WorldoutRect.size(),
+                 psd_m_output.rectInfo.label,
+                 psd_m_output.rectInfo.PStype,
+                 psd_m_output.rectInfo.iSodType,
+                 psd_m_output.rectInfo.StopperDistance,
+                 psd_m_output.rectInfo.StopperLocation,
+                 psd_m_output.rectInfo.pt[0].x,
+                 psd_m_output.rectInfo.pt[0].y,
+                 psd_m_output.rectInfo.pt[1].x,
+                 psd_m_output.rectInfo.pt[1].y,
+                 psd_m_output.rectInfo.pt[2].x,
+                 psd_m_output.rectInfo.pt[2].y,
+                 psd_m_output.rectInfo.pt[3].x,
+                 psd_m_output.rectInfo.pt[3].y);
+        } else if (slotType == "ORIGIN USSSLOTS") {
+            LOGD("[%s World] TOTAL SLOT NUM: %d, Slot#%d, type: %d, SOD: %d, DownSlotSOD: %d, iMinOtherSideDist: %d, iRoadEdgeDist: %d, (%d, %d) (%d, %d) (%d, %d) (%d, %d)",
+                 slotType.c_str(),
+                 slots.WorldoutRect.size(),
+                 psd_m_output.rectInfo.label,
+                 psd_m_output.rectInfo.PStype,
+                 psd_m_output.rectInfo.iSodType,
+                 psd_m_output.rectInfo.iDownSlotSOD,
+                 psd_m_output.rectInfo.iMinOtherSideDist,
+                 psd_m_output.rectInfo.iRoadEdgeDist,
+                 psd_m_output.rectInfo.pt[0].x,
+                 psd_m_output.rectInfo.pt[0].y,
+                 psd_m_output.rectInfo.pt[1].x,
+                 psd_m_output.rectInfo.pt[1].y,
+                 psd_m_output.rectInfo.pt[2].x,
+                 psd_m_output.rectInfo.pt[2].y,
+                 psd_m_output.rectInfo.pt[3].x,
+                 psd_m_output.rectInfo.pt[3].y);
+        } else if (slotType == "FUSIONSLOTS") {
+            LOGD("[%s World] TOTAL SLOT NUM: %d, Slot#%d, type: %d, occ: %d, StopDis: %f, StopLoc: %d, Material: %d, NotToRelease: %d, (%d, %d) (%d, %d) (%d, %d) (%d, %d)",
+                 slotType.c_str(),
+                 slots.WorldoutRect.size(),
+                 psd_m_output.rectInfo.label,
+                 psd_m_output.rectInfo.PStype,
+                 psd_m_output.rectInfo.iSodType,
+                 psd_m_output.rectInfo.StopperDistance,
+                 psd_m_output.rectInfo.StopperLocation,
+                 psd_m_output.rectInfo.iMaterial,
+                 psd_m_output.rectInfo.NotToRelease,
+                 psd_m_output.rectInfo.pt[0].x,
+                 psd_m_output.rectInfo.pt[0].y,
+                 psd_m_output.rectInfo.pt[1].x,
+                 psd_m_output.rectInfo.pt[1].y,
+                 psd_m_output.rectInfo.pt[2].x,
+                 psd_m_output.rectInfo.pt[2].y,
+                 psd_m_output.rectInfo.pt[3].x,
+                 psd_m_output.rectInfo.pt[3].y);
+        }
+    }
+}
+
+void ClearSelectRecommendSlot(int& HMI_temp_ID, int& HMI_select_ID, int& VCU_select_ID_ON, int& final_select_ID, int& RECOMMEND_ID, int& final_ID, int apa_status) {
+    HMI_temp_ID = 0;
+    HMI_select_ID = 0;
+    VCU_select_ID_ON = 0;
+    final_select_ID = 0;
+    RECOMMEND_ID = 0;
+    final_ID = 0;
+    LOGD("[STATUSSELECT] in ClearSelectRecommendSlot HMI %d, VCU %d, final select %d", HMI_temp_ID, VCU_select_ID_ON, final_select_ID);
+}
+
