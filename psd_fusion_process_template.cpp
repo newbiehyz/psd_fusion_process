d10430c83fa9ab882ca53be3e8140e8e9d8a8060
hongyi_zhu <hongyi_zhu@apa.patac>
2025-05-21 10:18:49 +0800

    add RDconfidence,toosmall

--- psd_fusion_process_template.cpp
+++ psd_fusion_process_template.cpp
@@ -569,7 +569,7 @@ tResult cpsd_fusion_process::TimeTrigger_thread_100ms_1()
     auto current1970_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current1970).count(); //用于J5时间同步
     auto start = std::chrono::steady_clock::now(); // 用于计算TIMECOST
 
-    LOGD("PSD Version: 05201015 emos10.0.2 add RDconfidence,toosmall,KFenable,FARAWAYconfig. DISABLE:psd2vcu fixed");
+    LOGD("PSD Version: 05211015 emos10.0.2 add RDconfidence,toosmall,KFenable,FARAWAYconfig. DISABLE:psd2vcu fixed");
     // GET方式获取
     rd::QuadParkingSlots rd_info;
     unsigned long long singleframeslotsID;
