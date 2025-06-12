9ce6b79d8dab4380480ecb0c267ba238e85f2acf
hongyi_zhu <hongyi_zhu@apa.patac>
2025-06-12 13:08:23 +0800

    version info update

--- psd_fusion_process_template.cpp
+++ psd_fusion_process_template.cpp
@@ -591,7 +591,7 @@ tResult cpsd_fusion_process::TimeTrigger_thread_100ms_1()
     auto current1970_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current1970).count(); //用于J5时间同步
     auto start = std::chrono::steady_clock::now(); // 用于计算TIMECOST
 
-    LOGD("PSD Version: 06111106 emos10.0.1 RTmatrix,fit with LYK: fusion, stopper, PStype, SOD");
+    LOGD("PSD Version: 06121307 emos10.0.1 [LYK]: PSD2CONTROL stopper,RTmatrix,fusion, stopper, PStype, SOD");
     // GET方式获取
     rd::QuadParkingSlots rd_info;
     unsigned long long singleframeslotsID;
