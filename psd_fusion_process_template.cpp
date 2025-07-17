c936e34f1b7ed36f1f522994492b1550f8ac2143
hongyi_zhu <hongyi_zhu@apa.patac>
2025-07-17 11:27:05 +0800

    0717 release

--- psd_fusion_process_template.cpp
+++ psd_fusion_process_template.cpp
@@ -650,7 +650,7 @@ tResult cpsd_fusion_process::TimeTrigger_thread_100ms_1()
     auto current1970_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current1970).count(); //用于J5时间同步
     auto start = std::chrono::steady_clock::now(); // 用于计算TIMECOST
 
-    LOGD("PSD Version: 07031511 emos10.0.1 0703 release add dignoal slot");
+    LOGD("PSD Version: 07171126 emos10.0.1 0717 release add dignoal slot");
     // GET方式获取
     rd::QuadParkingSlots rd_info;
     unsigned long long singleframeslotsID;
