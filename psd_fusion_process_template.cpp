93be4452890177f3ff7b10f416b0c53f350e75de
hongyi_zhu <hongyi_zhu@apa.patac>
2025-09-11 20:05:08 +0800

    support reverse direction stopper slot

--- psd_fusion_process_template.cpp
+++ psd_fusion_process_template.cpp
@@ -705,7 +705,7 @@ tResult cpsd_fusion_process::TimeTrigger_thread_100ms_1()
     auto current1970_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current1970).count(); //用于J5时间同步
     auto start = std::chrono::steady_clock::now(); // 用于计算TIMECOST
 
-    LOGD("PSD Version: 08281408 emos10.0.1 [LYK]: add psd2planning.targetslot id");
+    LOGD("PSD Version: 09111450 emos10.0.1 [LYK CUSTOM]: support reverse direction stopper slot");
     // GET方式获取
     rd::QuadParkingSlots rd_info;
     unsigned long long singleframeslotsID;
