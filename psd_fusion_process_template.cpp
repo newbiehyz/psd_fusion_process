f03454face29b131d5c4681e026b9f373a2857be
hongyi_zhu <hongyi_zhu@apa.patac>
2025-11-06 13:11:25 +0800

    fix stopperOBS VISandUSS dump

--- psd_fusion_process_template.cpp
+++ psd_fusion_process_template.cpp
@@ -642,7 +642,7 @@ tResult cpsd_fusion_process::TimeTrigger_thread_100ms_1()
     auto current1970_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current1970).count(); //用于J5时间同步
     auto start = std::chrono::steady_clock::now(); // 用于计算TIMECOST
 
-    LOGD("PSD Version: 10231314 emos10.0.1 no adjustOrder, no typeCorrect, enable reverse stopper, add USS bottomtype, lock targetslot");
+    LOGD("PSD Version: 11051012 emos10.0.1 fix stopperOBS VISandUSS dump");
     // GET方式获取
     rd::QuadParkingSlots rd_info;
     unsigned long long singleframeslotsID;
